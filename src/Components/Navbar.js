3886defd855aaeabd859876463e1f361b050231b
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-06-19 15:28:43 +0800

    Added website skeleton

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,66 @@
-import React from 'react'
-import {Outlet} from "react-router-dom"
+import React from "react";
+import { Outlet } from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>This is the navbar
-        <Outlet/>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content bg-base-200 flex flex-col items-center justify-center">
+        {/* Start of navbar */}
+        <div className="navbar bg-base-100 lg:hidden w-full">
+          <div className="flex-none">
+            <label htmlFor="my-drawer-2" className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          </div>
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <Outlet />
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 h-full bg-white text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
