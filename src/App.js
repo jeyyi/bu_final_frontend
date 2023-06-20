8c352341ad4c0d1064a72249ee8296773a595168
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-06-20 15:10:56 +0800

    Initial design

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./Components/Card";
 const App = () => {
   return (
     <>
-      <div className="w-full h-fit flex flex-col lg:flex-row gap-5">
+      <div className="w-full h-fit flex flex-col lg:flex-row gap-10">
         <Card />
         <Card />
         <Card />
@@ -12,7 +12,7 @@ const App = () => {
         <div className="w-2/3 h-full">
           <div className="w-full h-full bg-white rounded-xl shadow-sm"></div>
         </div>
-        <div className="w-1/3 h-full flex flex-col gap-5">
+        <div className="w-1/3 h-full flex flex-col gap-10">
           <div className="w-full h-1/2 bg-white rounded-xl shadow-sm"></div>
           <div className="w-full h-1/2 bg-white rounded-xl shadow-sm"></div>
         </div>
