6e60eb8f100ba92122f915c270c5daf7342f674e
Jayrhald Padilla <jayrhald.jp@gmail.com>
2023-08-22 10:45:55 +0800

    Fixed frontend

--- src/AnalyzeCSV.js
+++ src/AnalyzeCSV.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Logo from "./Assets/logo.png";
 import {
   BarChart,
@@ -20,6 +20,7 @@ function AnalyzeCSV() {
   // State to hold the subset of data
   const [subset, setSubset] = useState({});
   const [data, setData] = useState(null);
+  const fileInputRef = useRef(null);
   const CustomXAxisTick = ({ x, y, payload }) => {
     return (
       <g transform={`translate(${x},${y})`}>
@@ -39,36 +40,35 @@ function AnalyzeCSV() {
       </g>
     );
   };
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+  const handleFileChange = async (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      await handleSubmit(selectedFile);
+    }
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (selectedFile) => {
+    const formData = new FormData();
+    formData.append("file", selectedFile);
 
-    if (file) {
-      const formData = new FormData();
-      formData.append("file", file);
-
-      try {
-        const response = await axios.post(
-          "http://localhost:8000/analyzecsv",
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        );
-        console.log(response.data);
-        setData(response.data); // Process response from server
-        setTotalPages(
-          Math.ceil(Object.keys(response.data).length / itemsPerPage)
-        );
-        transformResponseData(response.data); // Transform response data
-      } catch (error) {
-        console.error(error);
-      }
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/analyzecsv",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(response.data);
+      setData(response.data); // Process response from server
+      setTotalPages(
+        Math.ceil(Object.keys(response.data).length / itemsPerPage)
+      );
+      transformResponseData(response.data); // Transform response data
+    } catch (error) {
+      console.error(error);
     }
   };
   const transformResponseData = (data) => {
@@ -103,21 +103,10 @@ function AnalyzeCSV() {
 
   return (
     <>
-      <h2>Upload CSV File</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          onChange={handleFileChange}
-          accept=".csv,.xlsx,.xls"
-        />
-        <button type="submit" className="btn btn-primary">
-          Upload
-        </button>
-      </form>
       <div className="w-full h-40 my-10 bg-base-100 shadow-sm flex">
         <div className="w-full h-full flex flex-col">
           <h3 className="px-10 pt-10 text-green-700 text-xl lg:text-4xl font-bold">
-            ANAL<span className="text-green-600">YSE</span>
+            ANA<span className="text-green-600">LYSE</span>
           </h3>
           <h3 className="px-10 text-green-700 text-2xl lg:text-6xl font-bold">
             EXCEL<span className="text-green-600"> FILE</span>
@@ -125,51 +114,68 @@ function AnalyzeCSV() {
         </div>
         {/* Cover photo */}
         <div className="h-full w-44 lg:w-72 flex items-center">
-          {
-            <img
-              src={Logo}
-              className="w-full h-full mx-auto object-contain lg:object-cover pr-5 lg:pr-0"
-              alt="excel logo"
-            />
-          }
+          <img
+            src={Logo}
+            className="w-full h-full mx-auto object-contain lg:object-cover pr-5 lg:pr-0 cursor-pointer"
+            alt="excel logo"
+            onClick={() => fileInputRef.current.click()}
+          />
+          <input
+            type="file"
+            onChange={handleFileChange}
+            accept=".csv,.xlsx,.xls"
+            style={{ display: "none" }}
+            ref={fileInputRef}
+          />
         </div>
       </div>
       <div className="divider" />
-      <h3 className="text-gray-700 font-semibold text-2xl pb-6">Analysis</h3>
+
       <div className="flex flex-col gap-3">
         {/* Question Card dropdown details summary */}
-        {transformedData &&
-          Object.keys(subset).map((question, index) => {
-            const dataForQuestion = transformedData.filter(
-              (item) => item.question === question
-            );
-            return (
-              <div
-                key={index}
-                tabIndex={0}
-                className="collapse collapse-arrow bg-blue-300 shadow-sm"
-              >
-                <div className="collapse-title">{question}</div>
-                <div className="collapse-content bg-white">
-                  <div className="p-0 lg:p-10">
-                    <ResponsiveContainer width="100%" height={300}>
-                      <BarChart data={dataForQuestion} layout="vertical">
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis type="number" tick={<CustomXAxisTick />} />
-                        <YAxis
-                          type="category"
-                          dataKey="name"
-                          tick={<CustomYAxisTick />}
-                        />
-                        <Tooltip />
-                        <Bar dataKey="value" fill="#1498FF" />
-                      </BarChart>
-                    </ResponsiveContainer>
+        {transformedData ? (
+          <>
+            <h3 className="text-gray-700 font-semibold text-2xl pb-6">
+              Analysis
+            </h3>
+            {Object.keys(subset).map((question, index) => {
+              const dataForQuestion = transformedData.filter(
+                (item) => item.question === question
+              );
+              return (
+                <div
+                  key={index}
+                  tabIndex={0}
+                  className="collapse collapse-arrow bg-blue-300 shadow-sm mx-0"
+                >
+                  <div className="collapse-title text-sm flex items-center">
+                    {question}
+                  </div>
+                  <div className="collapse-content bg-white">
+                    <div className="p-0 lg:p-10">
+                      <ResponsiveContainer width="100%" height={300}>
+                        <BarChart data={dataForQuestion} layout="vertical">
+                          <CartesianGrid strokeDasharray="3 3" />
+                          <XAxis type="number" tick={<CustomXAxisTick />} />
+                          <YAxis
+                            type="category"
+                            dataKey="name"
+                            tick={<CustomYAxisTick />}
+                          />
+                          <Tooltip />
+                          <Bar dataKey="value" fill="#1498FF" />
+                        </BarChart>
+                      </ResponsiveContainer>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </>
+        ) : (
+          <h3 className="text-center my-10 font-semibold text-2xl lg:text-4xl text-gray-600">Analyze CSV by clicking on "excel" icon</h3>
+        )}
+
         {/*   <div className="flex justify-between mt-4">
           <button
             className="btn btn-primary"
